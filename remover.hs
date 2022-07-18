028516d5c9f7dc08acb9352d650f542313791d7f
Myname is pizza <pirogovde@mer.ci.nsu.ru>
2022-07-18 02:23:29 -0400

    now remover.hs will solve step 8 of IO Monad steps on stepik haskell course

--- remover.hs
+++ remover.hs
@@ -2,16 +2,19 @@ import System.Directory
 import Data.List
 import System.FilePath
 
+{-In this program you must enter a string when you
+asked to and this will delete all files from
+the current directory for whom the string you entered
+is a substring-}
+
 main' = do 
     putStr "Substring: "    
-    subString <- getLine -- "remo"
-    if subString == "\n" then putStr "\n cancelled"
+    subString <- getLine -- "hell"
+    if subString == "" then putStrLn "Canceled"
     else do 
-        directory <- getCurrentDirectory -- "/home/dick/Templates/Haskell/monads/functions/remover"
-        contents <- listDirectory directory -- ["remover.hs","something.hs"]
+        --directory <- getCurrentDirectory -- "/home/dick/Templates/Haskell/monads/functions/remover"
+        contents <- getDirectoryContents "." -- ["remover.hs","something.hs","hello.world"]
         check contents subString
-
-type Substring = String
 
 check :: [String] -> String -> IO () 
 check [] _ = return ()
@@ -21,6 +24,7 @@ check (x:xs) subStr = checked where
         then do
             putStrLn $ "Removing file: " ++ x 
             removeFile x
+            check xs subStr
         else check xs subStr
     pref a = subStr `isPrefixOf` a
     suff a = subStr `isSuffixOf` a
